62af154286dc2d74b17736c59bb6a2cc6e012d87
Yash Ghelani <yashghelani08@gmail.com>
2020-05-20 20:53:38 +0100

    latest files - delete old proto file and use new one

--- COM2009_Assignment_2/controllers/new_robot_avoid/new_robot_avoid.py
+++ COM2009_Assignment_2/controllers/new_robot_avoid/new_robot_avoid.py
@@ -1,34 +1,69 @@
 from controller import Robot, Motor, DistanceSensor
+import math
 
 TIME_STEP = 64
+SPEED = 7.0
 robot = Robot()
 
 ds = []
-dsNames = ['ds_right', 'ds_left']
+dsNames = ['ds_front_right', 'ds_front_left', 'ds_right1', 'ds_right2']
+ls = []
+lsNames = ['ls_right', 'ls_left']
 
-for i in range(2):
+for i in range(4):
     ds.append(robot.getDistanceSensor(dsNames[i]))
     ds[i].enable(TIME_STEP)
     
 wheels = []
 wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4']
+
 for i in range(4):
     wheels.append(robot.getMotor(wheelsNames[i]))
     wheels[i].setPosition(float('inf'))
     wheels[i].setVelocity(0.0)
-avoidObstacleCounter = 0
-while robot.step(TIME_STEP) != -1:
-    leftSpeed = 1.0
-    rightSpeed = 1.0
-    if avoidObstacleCounter > 0:
-        avoidObstacleCounter -= 1
-        leftSpeed = 1.0
-        rightSpeed = -1.0
-    else:  # read sensors
-        for i in range(2):
-            if ds[i].getValue() < 950.0:
-                avoidObstacleCounter = 100
+    
+def turn(count, direction):
+    print(ds[2].getValue(), ds[3].getValue())
+    while robot.step(TIME_STEP) != -1 and count > 0:
+    
+        if direction == "l":
+            leftSpeed = SPEED * -1
+            rightSpeed = SPEED
+        elif direction == "r":
+            leftSpeed = SPEED
+            rightSpeed = SPEED * -1
+        elif direction == "s":
+            leftSpeed = SPEED
+            rightSpeed = SPEED
+        else:
+            leftSpeed = SPEED * -1
+            rightSpeed = SPEED
+            
+        wheels[0].setVelocity(leftSpeed)
+        wheels[1].setVelocity(leftSpeed)
+        wheels[2].setVelocity(rightSpeed)
+        wheels[3].setVelocity(rightSpeed)
+        count -= 1
+        print("turning")
+        
+while robot.step(TIME_STEP) != -1: 
+      
+    leftSpeed = SPEED
+    rightSpeed = SPEED
+    
+    if ds[0].getValue() < 850 or ds[1].getValue() < 850:
+        turn(10, "l")
+    
+    #if math.floor(ds[2].getValue() / 10) < ds[3].getValue():
+    
+    if ds[2].getValue() < ds[3].getValue():
+        turn(1, "l")
+    
+    if ds[2].getValue() > ds[3].getValue():
+        turn(1, "r")
+                         
     wheels[0].setVelocity(leftSpeed)
-    wheels[1].setVelocity(rightSpeed)
-    wheels[2].setVelocity(leftSpeed)
+    wheels[1].setVelocity(leftSpeed)
+    wheels[2].setVelocity(rightSpeed)
     wheels[3].setVelocity(rightSpeed)
+    
